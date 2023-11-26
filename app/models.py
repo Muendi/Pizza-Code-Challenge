f515445c87625c2d709040c1ff0f60622302a47b
Michelle Kyalo <muendikyalo@gmail.com>
2023-11-26 23:48:46 +0300

    Added restaurant model

--- app/models.py
+++ app/models.py
@@ -6,5 +6,11 @@ class Restaurant(db.Model):
     __tablename__ = 'Restaurant'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    pizzas = db.relationship('Pizza', secondary='restaurant_pizza', backref='Restaurant')
+
+
+
 
 # add any models you may need. 
