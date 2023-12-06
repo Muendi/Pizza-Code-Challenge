c58372782fc669b040e511edb79bb60a3e78d9b7
Michelle Kyalo <muendikyalo@gmail.com>
2023-12-06 13:38:54 +0300

    Added route for all restaurants

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, jsonify, request #make_response
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from models import db, Restaurant
+from models import db, Restaurant, Pizza, RestaurantPizza
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
@@ -16,6 +17,20 @@ db.init_app(app)
 @app.route('/')
 def home():
     return ''
+
+# Route to get all restaurants
+@app.route('/restaurants', methods=['GET'])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    formatted_restaurants = [
+        {
+            "id": restaurant.id,
+            "name": restaurant.name,
+            "address": restaurant.address
+        }
+        for restaurant in restaurants
+    ]
+    return jsonify(formatted_restaurants)
 
 
 if __name__ == '__main__':
